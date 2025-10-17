967f01d52f2cca7ae9e6a8f8c2a420381e8aa482
Your Name <2511500073@mahasiswa.atmaluhur.ac.id>
2025-10-17 19:32:09 +0700

    index.html

--- pertemuan-04/script.js
+++ pertemuan-04/script.js
@@ -6,3 +6,39 @@
   });
 
 
+
+
+    const phrases = [
+      "Hello, I'm a Developer.",
+      "I build cool web apps.",
+      "Welcome to my website!",
+      "Let's create something amazing."
+    ];
+
+    const typingElement = document.getElementById("typing");
+    let phraseIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+
+    function type() {
+      const currentPhrase = phrases[phraseIndex];
+      const currentText = currentPhrase.substring(0, charIndex);
+
+      typingElement.textContent = currentText;
+
+      if (!isDeleting && charIndex < currentPhrase.length) {
+        charIndex++;
+        setTimeout(type, 100);
+      } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+        setTimeout(type, 50);
+      } else {
+        isDeleting = !isDeleting;
+        if (!isDeleting) {
+          phraseIndex = (phraseIndex + 1) % phrases.length;
+        }
+        setTimeout(type, 1000);
+      }
+    }
+
+    type();
